3645c5c0274965198423396db5af9d1509e3f4b8
JustShooter <milicia@tut.by>
2022-01-01 23:36:01 +0300

    HomeWork16

---
+++ src/HomeWork16/Task1/Model/NewProduct.java
@@ -0,0 +1,37 @@
+/**
+ * NewProduct class
+ */
+package HomeWork16.Task1.Model;
+
+import java.util.Objects;
+
+public class NewProduct {
+    private final int toySize;
+    private final int toyPrice;
+
+    public NewProduct(final int toySize, final int toyPrice) {
+        this.toySize = toySize;
+        this.toyPrice = toyPrice;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        final NewProduct newProduct = (NewProduct) o;
+        return this.toySize == newProduct.toySize && this.toyPrice == newProduct.toyPrice;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.toySize, this.toyPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "NewProduct{" +
+                "toySize=" + toySize +
+                ", toyPrice=" + toyPrice +
+                '}';
+    }
+}
